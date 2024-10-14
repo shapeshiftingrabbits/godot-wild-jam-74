d70821afb8765273b391f9bd7a609743971d3b25
Laurie Trichet <laurie.trichet@protonmail.com>
2024-10-14 13:49:14 +1100

    remove comments

--- player.gd
+++ player.gd
@@ -17,7 +17,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	print("input_dir = ", input_dir)
+	
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -25,5 +25,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	print("velocity = ", velocity)
+	
 	move_and_slide()
