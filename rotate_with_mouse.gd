6ac07eb2285281b18a4f857374c24b55064359c1
Laurie Trichet <laurie.trichet@protonmail.com>
2024-10-15 09:07:54 +1100

    make the haunting indicator follow the mouse.

--- rotate_with_mouse.gd
+++ rotate_with_mouse.gd
@@ -2,26 +2,47 @@ extends Node3D
 
 @onready var camera_3d: Camera3D = $"../Camera3D"
 @onready var character_body_3d: CharacterBody3D = $".."
-@onready var mouse_ray_cast_3d: RayCast3D = $"../Camera3D/MouseRayCast3D"
+@onready var mouse_ray_cast_3d: RayCast3D = %MouseRayCast3D
 
-#func _ready() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-var mouse_position : Vector2
+@onready var csg_box_3d: CSGBox3D = $"../../CSGBox3D"
 
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		mouse_position = event.position
-		#in_world_mouse_position.y = 1.0
-	else:
-		mouse_position = Vector2.ZERO
-	
+const RAY_LENGTH = 1000
+	#
 
 func _physics_process(delta: float) -> void:
-	var from = camera_3d.project_ray_origin(mouse_position)
-	var to = from + camera_3d.project_ray_normal(mouse_position) * 1000.0
-	if (mouse_ray_cast_3d.is_colliding()):
-		var collision_point = mouse_ray_cast_3d.get_collision_point()
-		print(collision_point)
-		look_at(collision_point )
-		
+	#test_collision_with_query()
+	test_collision_with_ray()
+
+
+func test_collision_with_query():
+	var mouse_pos = get_viewport().get_mouse_position()
+
+	var from = camera_3d.project_ray_origin(mouse_pos)
+	var to = from + camera_3d.project_ray_normal(mouse_pos) * RAY_LENGTH
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(from, to)
+	#query.collide_with_areas = true
+#
+	var result = space_state.intersect_ray(query)
+	if result:
+		csg_box_3d.position = result.position
+		look_at(result.position)
+	
+	
+	
+func test_collision_with_ray():
+	var mouse_pos = get_viewport().get_mouse_position()
+
+	var from = camera_3d.project_ray_origin(mouse_pos)
+	var to = from + camera_3d.project_ray_normal(mouse_pos) * RAY_LENGTH
+	
+	mouse_ray_cast_3d.position = from
+	mouse_ray_cast_3d.target_position = to
+	
+	if mouse_ray_cast_3d.is_colliding():
+		var look_at_position = mouse_ray_cast_3d.get_collision_point()
+		print(look_at_position)
+		csg_box_3d.position = look_at_position
+		look_at(look_at_position)
+	
