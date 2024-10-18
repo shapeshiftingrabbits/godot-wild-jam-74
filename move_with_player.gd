a83e20cf81d7ade12b4a6f9c759b0d8eb7fa44ec
Laurie Trichet <laurie.trichet@protonmail.com>
2024-10-18 22:40:43 +1100

    add work around for shrinking the tooltip

--- move_with_player.gd
+++ move_with_player.gd
@@ -15,3 +15,9 @@ func _get_position_offsetted(position: Vector2) -> Vector2:
 	position_offsetted.x = position.x + 24.0
 	position_offsetted.y = position.y + 24.0
 	return position_offsetted
+
+
+func _on_v_box_container_resized() -> void:
+	# This is because the panel is not shrinking at runtime, in editor is fine.
+	reset_size()
+	#pass
