480d53f8e0ac4bec450d83614108212bd1b58a30
Laurie Trichet <laurie.trichet@protonmail.com>
2024-10-18 21:26:53 +1100

    attempt to make the container for the tooltip hug content
    add an offset to the tooltip position

--- move_with_player.gd
+++ move_with_player.gd
@@ -3,6 +3,15 @@ extends Control
 @onready var player: Player = $"../Player"
 @onready var camera_3d: Camera3D = $"../Camera3D"
 
+
+var position_offsetted = Vector2(0,0)
+
 func _physics_process(delta: float) -> void:
 	var position_on_screen = camera_3d.unproject_position(player.position)
-	position = position_on_screen
+	position = _get_position_offsetted(position_on_screen)
+
+
+func _get_position_offsetted(position: Vector2) -> Vector2:
+	position_offsetted.x = position.x + 24.0
+	position_offsetted.y = position.y + 24.0
+	return position_offsetted
